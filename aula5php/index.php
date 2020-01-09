33a0e2a2bb2d9dca3184a005539edda474521d8f
Técnico em Informática 2019.6 <16121742019.6@senacrjedu.local>
2020-01-09 11:13:59 -0300

    Arquivos php

---
+++ aula5php/index.php
@@ -0,0 +1,43 @@
+<html>
+ <head>
+ <meta http-equiv="content-type" content="text/html;
+ charset=UTF-8">
+<title>Cadastro de clientes</title>
+ </head>
+ <body>
+ <h3> Sistema de cliente</h3>
+ <form action = ""method="post">
+ Nome..:
+ <br>
+ <input type="text" name = "Nome"/>
+ <br><br>
+  Email..:
+ <br>
+ <input type="text" name = "Email"/>
+ <br><br>
+  Idade..:
+ <br>
+ <input type="text" name = "Idade"/>
+ <br><br>
+  Estado civil..:
+ <br>
+<select name ="estado">
+<option value = "">Selecione --</option>
+<option value = "Solteiro">Solteiro --</option>
+<option value = "Casado">Casado --</option>
+<option value = "Viuvo">Viúvo --</option>
+<option value = "Divorciado">Divorciado --</option>
+ <br><br>
+ </select>
+ <br><br>
+ <input type="submit" value="Cadastrar Cliente">
+ <input type="reset" value="Limpar Campos">
+ 
+ 
+ 
+ 
+ 
+ </form>
+ </body>
+ 
+ </html>
